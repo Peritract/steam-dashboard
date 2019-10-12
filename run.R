06b643ec084cd5b6140e2252b8674def89f88dbe
Peritract <peritract@hotmail.co.uk>
2019-10-12 17:40:46 +0100

    server rearrangement

--- run.R
+++ run.R
@@ -5,9 +5,8 @@ port <- Sys.getenv('PORT')
 # Runs the app in the working directory. By default this will search for an `app.R` file or 
 # a ui.R + server.R file combination
 # The host of 0.0.0.0 indicates the app runs at the web server's ip address
-#shiny::runApp(
- # appDir = getwd(),
-  #host = '0.0.0.0',
-  #port = as.numeric(port)
-#)
-rmarkdown::run()
+shiny::runApp(
+  appDir = getwd(),
+  host = '0.0.0.0',
+  port = as.numeric(port)
+)
